fbde4178ed284a6795d4f15491d4db646c973a35
wilfrank84 <wilfrank84@gmail.com>
2017-09-12 22:45:12 -0300

    Adding action destroy in the users

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -99,6 +99,22 @@ RSpec.describe 'Users API', type: :request do
 				expect(user_response).to have_key(:errors)
 			end
 		end
-	end	
+	end
+
+	# Test routine delete users
+	describe 'DELETE /users/:id' do
+		before do
+			headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
+			delete "/users/#{user_id}", params: {}, headers: headers
+		end
+
+		it 'returns status code 204' do
+			expect(response).to have_http_status(204)
+		end
+
+		it 'removes the user from database' do
+			expect( User.find_by(id: user.id) ).to be_nil
+		end
+	end
 
 end
