f5fedf0dc84966528bd14782bc776e8e36aba0e2
wilfrank84 <wilfrank84@gmail.com>
2017-09-20 17:47:58 -0300

    Adding show action to the tasks controller with tests

--- spec/requests/api/v1/tasks_spec.rb
+++ spec/requests/api/v1/tasks_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe 'Task API' do
 		}
 	end
 
-
+	# Test routine of the index action
 	describe 'GET /tasks' do
 	  before do
 	  	create_list(:task, 5, user_id: user.id)
@@ -28,5 +28,21 @@ RSpec.describe 'Task API' do
 	  	expect(json_body[:tasks].count).to eq(5)
 	  end
 	end
+
+	# Test routine of the show action
+	describe 'GET /tasks/:id' do
+	  let(:task) { create(:task, user_id: user.id) }
+	
+	  before { get "/tasks/#{task.id}", params: {}, headers: headers }
+
+	  it 'returns status code 200' do
+	  	expect(response).to have_http_status(200)
+	  end
+
+	  it 'returns the json for task' do
+	  	expect(json_body[:title]).to eq(task.title)
+	  end
+	end
+
 
 end
