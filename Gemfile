048254b41e99a593a9f4f9c1aa88467428cfd285
wilfrank84 <wilfrank84@gmail.com>
2017-09-22 13:24:48 -0300

    Implementing JSONAPI.ORG patterns for tasks

--- Gemfile
+++ Gemfile
@@ -51,3 +51,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 
 gem 'devise'
+gem 'active_model_serializers', '~> 0.10.0'
+
+
--- spec/requests/api/v2/tasks_spec.rb
+++ spec/requests/api/v2/tasks_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe 'Task API' do
 	  end
 
 	  it 'returns 5 tasks from database' do
-	  	expect(json_body[:tasks].count).to eq(5)
+	  	expect(json_body[:data].count).to eq(5)
 	  end
 	end
 
@@ -40,7 +40,7 @@ RSpec.describe 'Task API' do
 	  end
 
 	  it 'returns the json for task' do
-	  	expect(json_body[:title]).to eq(task.title)
+	  	expect(json_body[:data][:attributes][:title]).to eq(task.title)
 	  end
 	end
 
@@ -62,11 +62,11 @@ RSpec.describe 'Task API' do
 	  	end
 
 	  	it 'returns the json for created task' do
-	  		expect(json_body[:title]).to eq(task_params[:title])
+	  		expect(json_body[:data][:attributes][:title]).to eq(task_params[:title])
 	  	end
 
 	  	it 'assigns the created task to the current user' do
-	  		expect(json_body[:user_id]).to eq(user.id)
+	  		expect(json_body[:data][:attributes][:'user-id']).to eq(user.id)
 	  	end
 	  end
 
@@ -103,7 +103,7 @@ RSpec.describe 'Task API' do
 	  	end
 
 	  	it 'returns the json for update task' do
-	  		expect(json_body[:title]).to eq(task_params[:title])
+	  		expect(json_body[:data][:attributes][:title]).to eq(task_params[:title])
 	  	end
 
 	  	it 'updates the task in the database' do
